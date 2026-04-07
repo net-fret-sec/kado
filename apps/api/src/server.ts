20e7016b969c8c9297c3dfc42708c2c2a0b36470
Dave Létourneau <netfretsec@gmail.com>
2026-04-07 15:53:49 -0400

    listen backend

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,13 +1,14 @@
-import 'dotenv/config'
-import { createApp } from './app'
-import { loadTestData } from './load-test-data'
+import "dotenv/config";
+import { createApp } from "./app";
+import { loadTestData } from "./load-test-data";
 
-const PORT = Number(process.env.SERVER_PORT) || 3000
+const SERVER_ADDRESS = process.env.SERVER_ADDRESS || "http://0.0.0.0";
+const PORT = Number(process.env.SERVER_PORT) || 3000;
 
-const app = createApp()
+const app = createApp();
 
-loadTestData()
+loadTestData();
 
 app.listen(PORT, () => {
-  console.log(`API listening on http://localhost:${PORT}`)
-})
+  console.log(`API listening on ${SERVER_ADDRESS}:${PORT}`);
+});
